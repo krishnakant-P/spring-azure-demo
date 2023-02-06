e181221a0cd060ed9e6338b1ddc9ddfb3c48b665
krishnakantpatel <krishnapatel0616@gmail.com>
2023-02-06 16:51:44 +0530

    first commit

---
+++ src/test/java/com/spring/azure/demo/AzureSpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.azure.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureSpringbootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
